55d18fb9190de9d427f0a048ca8bfab4af3cf790
Kanishk Gupta <kanishkgupta1234@gmail.com>
2020-05-16 04:53:39 +0530

    Adding files

---
+++ src/Jokescard.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import {Card,CardContent,CardActions,CssBaseline,Container,Typography,Button,Chip,Tabs,Tab, AppBar} from '@material-ui/core';
+import {makeStyles,withStyles} from '@material-ui/core/styles';
+
+const styles=makeStyles({
+    card:{
+        marginBottom:20,
+        marginTop:20,
+        backgroundColor:'#ffd31d',
+        color:'#000000'
+    },
+    cContent:{
+        padding:12
+    },
+    cardAction:
+    {
+        padding:18
+    },
+    button:{
+        backgroundColor:'#f57b51'
+    }  
+})
+
+export default function Jokescard({joke,like,unLike,index}) {
+    const classes=styles();
+    const Type =withStyles({
+        root:{
+            marginTop:10,
+            marginBottom:10,
+            backgroundColor:'#e7d39f',
+            color:'#000000'
+        }
+        })(Chip);
+    return (
+        <Card className={classes.card} id={`joke-${index}`}>
+            <CardContent className={classes.cContent}>
+            {joke.categories.length>0?(
+                joke.categories.map((type)=>(
+                <Type label={type} key={type} variant='outlined' color='primary'/>
+                ))
+            ):<Type label='regular' variant='outlined' color='primary'/>}
+            <Typography>
+                {joke.joke}
+            </Typography>
+            </CardContent>
+            <CardActions className={classes.cardAction}>
+            <Button variant='contained' onClick={()=>like(joke.id)} style={{backgroundColor:"#f57b51"}}>
+                Like #karona😂
+            </Button>
+            <Button variant='contained' color='default' onClick={()=>unLike(joke.id)} style={{backgroundColor:"#ffc38b"}}>
+                Unlike😖
+            </Button>
+            </CardActions>            
+        </Card>
+    )
+}
